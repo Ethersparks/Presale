31d6e373ba649a8404598be6098514465fb9eab8
antoineMica <antoine.micaelian@gmail.com>
2018-05-06 01:49:25 -0700

    removed unused code

--- app/components/App.js
+++ app/components/App.js
@@ -17,45 +17,8 @@ class App extends React.Component {
             showWPHtml:false,
             wpPost:undefined,
         };
-
-        
-        this.headerStyle ={
-            color: '#575757',
-            maxWidth: '1100px',
-            top: 'auto',
-            height: '90px',
-            position: 'relative',
-            margin: '0 auto',
-        };
-
-        this.linkContainer ={
-            textAlign: 'center',
-            //backgroundImage: url("http://ethersparks.io/wp-content/uploads/2018/04/100620-digital-backgrounds-2880x1800-720p.jpg"),
-            paddingTop: '0px',
-            paddingBottom: '0px',
-            backgroundPosition: 'center 18px',
-            backgroundAttachment: 'fixed',
-        };
-
-        this.footerStyle ={
-            textAlign: 'center',
-            //backgroundImage: url("http://ethersparks.io/wp-content/uploads/2018/04/100620-digital-backgrounds-2880x1800-720p.jpg"),
-            paddingTop: '0px',
-            paddingBottom: '0px',
-            backgroundPosition: 'center 18px',
-            backgroundAttachment: 'fixed',
-        };
-
-        this.imageStyle ={
-            backgroundImage: bannerImage,
-            maxHeight: '80px',
-            width: 'auto',
-            height: 'auto',
-            position: 'relative'
-        };
     };
     
-  
     setExternalHTML(vis) {
         this.setState({showWPHtml: vis});
     };
@@ -65,42 +28,23 @@ class App extends React.Component {
         fetch(dataURL)
         .then(res => res.json())
         .then(res => {
-            console.log(res.post);
             this.setState({wpPost:res.post});
             this.setExternalHTML(true);
         })
 
     }
-
-    // getChildContext(){
-    //   return {
-    //     muiTheme: getMuiTheme()
-    //   };
-    // }
-    
-    // <AppBar 
-    // style = {this.styles}
-    // iconElementLeft ={<img src={bannerImage} style={this.imageStyle}/>}
-    // />
     getwpPost() {
         return {__html: this.state.wpPost};
     }
     render() {
-        // if (this.wpPost) {
-        //     return ( 
-        //         <div> this.wpPost </div>
-        //     )
-        // } else
-        {
-            return (
-                <div>
-                {this.state.showWPHtml ? <div>
-                  <div dangerouslySetInnerHTML={this.getwpPost()} ></div>
-                </div> : null}
-              </div>
-            );
-        }
-    }
+        return (
+            <div>
+            {this.state.showWPHtml ? <div>
+                <div dangerouslySetInnerHTML={this.getwpPost()} ></div>
+            </div> : null}
+            </div>
+        );
+    }    
 }
 
 

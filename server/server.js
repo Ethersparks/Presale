97194e02582b3b56f1f94a54e78d796d1c92a354
antoineMica <antoine.micaelian@gmail.com>
2018-05-10 22:02:21 -0700

    Worrking database conenction, created new users table and small interface for querying mysql

--- server/server.js
+++ server/server.js
@@ -5,32 +5,93 @@ const mysql = require("mysql")
 
 
 class AwsService {
-  constructor() {
-
-      
-      this.mysqlDB = mysql.createConnection({
+  constructor() {      
+      this.mysqlDB = mysql.createPool({
+          connectionLimit : 10,
           host:'ethersparkuserbase.clw8hn9uuxrr.us-west-1.rds.amazonaws.com',
           port:'3306',
           user: '',
           password: '',
           database: 'Users',
       });
-      this.mysqlDB.connect();
-      console.log(this.mysqlDB);
+      this.connected = false;
   }
 
-  createTable(name) {
-      //
+  connect() {
+    this.mysqlDB.connect(function(err) {
+      if (err) {
+        this.connected = false;
+        console.error('Database connection failed: ' + err.stack);
+        return;
+      }
+      this.connected = true;
+      console.log('Connected to database.');
+    });
   }
+
+  close() {
+    this.connected = false;
+    this.mysqlDB.end(function(err) {
+      // The connection is terminated now
+    });
+  }
+  //Example Query, values
+  //"UPDATE users SET foo = ?, bar = ?, baz = ? WHERE id = ?", ['a', 'b', 'c', userId]
+  executeQuery(queryString, values, successCallback) {
+    if (!values) {
+      values = [];
+    }
+
+    if( !Array.isArray(values)) {
+      values = [values];
+    }
+   
+    this.mysqlDB.getConnection((error, connection) => {    
+
+      if (error) throw error;
+      this.mysqlDB.query({
+        sql: queryString,
+        timeout: 40000, // 40s
+      },
+      values,
+      function (error, results, fields) {
+        // Done with this connection.
+        connection.release();
+        // error will be an Error if one occurred during the query
+        // results will contain the results of the query
+        // fields will contain information about the returned results fields (if any)
+        if (error) throw error;
+
+        
+        console.log("Query Success");
+
+        if(successCallback) {
+          console.log("Calling Callback");
+          successCallback();
+        }
+      });
+    });
+    
+
+    return true;
+  }
+
+  // createTable(tableName) {
+  //   const queryString = `
+  //         CREATE TABLE IF NOT EXISTS ${tableName}
+  //         (PRESALE_ID INT AUTO_INCREMENT NOT NULL, EMAIL VARCHAR(512) NOT NULL, PUBLIC_ETH_ADDRESS VARCHAR(512) NOT NULL,
+  //         IS_DELETED CHAR(1), CREATED_AT DATETIME DEFAULT CURRENT_TIMESTAMP,UPDATED_AT DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,PRIMARY KEY (PRESALE_ID))	
+  //   `;
+  //   const values = "";
+  //   this.executeQuery(queryString, values);
+  //   return true;
+  // }
 
 }
 
-
+const awsServ = new AwsService();
 // Create server
 var server = express();
-server.use(bodyParser.json());
-
-const awsServ = new AwsService();
 
 // Middleware
 server.use(function (req, res, next) {
@@ -38,11 +99,15 @@ server.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  
+  server.use(bodyParser.json());
+
   next();
 });
 
 server.post('/user', function(req, res, next) {
   var user = req.body;
+  
   
   console.log(user)
   res.send({
